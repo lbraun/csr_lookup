c9c0eea17a42eb76f07bba1953efc20baeb5c11c
Lucas Braun <lucasalan@gmail.com>
2017-10-22 21:34:29 +0200

    Remove unused app folder

--- app/scripts/index.js
+++
@@ -1,9 +0,0 @@
-angular.module('csrApp', [])
-      .controller('TestController', ['$scope', function ($scope) {
-        $scope.greetMe = 'World';
-		$scope.mainTitle = "CSR Lookup";
-      }]);
-
-    // angular.element(function() {
-      // angular.bootstrap(document, ['csrApp']);
-    // });
