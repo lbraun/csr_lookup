13ae5b0f6f95e13b4e7388c54d4968430e3e9fc0
alaacs <alaa.cs@hotmail.com>
2017-10-22 22:01:58 +0200

    Modify node_proof_of_concept.js to handle the DB with password

--- node_proof_of_concept.js
+++ node_proof_of_concept.js
@@ -20,8 +20,9 @@ const app = express()
 var pg = require('pg')
 var format = require('pg-format')
 var PGUSER = process.argv[2]
+var PGPASSWORD = process.argv[3]
 var PGDATABASE = 'csr_lookup'
-var companyName = process.argv[3]
+var companyName = process.argv[4]
 
 if (PGUSER == undefined || companyName == undefined) {
   console.log("Error: arguments must not be blank")
@@ -29,6 +30,7 @@ if (PGUSER == undefined || companyName == undefined) {
 } else {
   var config = {
     user: PGUSER,            // name of the user account
+	password:PGPASSWORD,	 // password of the user account
     database: PGDATABASE,    // name of the database
     max: 10,                 // max number of clients in the pool
     idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
@@ -46,6 +48,7 @@ if (PGUSER == undefined || companyName == undefined) {
 
     myClient = client
     var companyQuery = format('SELECT * FROM companies WHERE name = %L', companyName)
+	
 
     myClient.query(companyQuery, function (err, result) {
       if (err) console.log(err)
