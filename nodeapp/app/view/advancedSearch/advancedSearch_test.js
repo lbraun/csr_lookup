801b48427251186f8d58d9bf734ffcc49f8837a0
Lucas Braun <lucasalan@gmail.com>
2017-11-21 19:46:12 +0100

    Fix tests that require use of $scope

--- nodeapp/app/view/advancedSearch/advancedSearch_test.js
+++ nodeapp/app/view/advancedSearch/advancedSearch_test.js
@@ -5,9 +5,14 @@ describe('csrLookupApp.advancedSearch module', function() {
   beforeEach(module('csrLookupApp.advancedSearch'));
 
   describe('advancedSearch controller', function(){
+    var scope, advancedSearchCtrl;
+
+    beforeEach(inject(function($rootScope, $controller) {
+      scope = $rootScope.$new();
+      advancedSearchCtrl = $controller('AdvancedSearchCtrl', {$scope: scope});
+    }));
 
     it('should be defined', inject(function($controller) {
-      var advancedSearchCtrl = $controller('AdvancedSearchCtrl');
       expect(advancedSearchCtrl).toBeDefined();
     }));
 
