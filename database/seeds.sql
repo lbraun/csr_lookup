5c8f39f049acbd103077e3b6e0a7ce6f5376296c
Lucas Braun <lucasalan@gmail.com>
2017-11-22 13:57:13 +0100

    Add more seed data, improve db management

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,13 +1,35 @@
-INSERT INTO users (name) VALUES
-  ('Alaa'),
-  ('Lucas');
+-- psql csr_lookup < database/seeds.sql
 
-INSERT INTO companies (name, wikipedia_name, industry) VALUES
-  ('ESRI', 'Esri', 'software'),
-  ('Mercadona', 'Mercadona', 'retail');
+INSERT INTO
+  users
+VALUES
+  (1, 'Alaa'),
+  (2, 'Lucas');
 
-INSERT INTO evidence_records (title, reference_url, reference_title, description, score, fk_created_by, fk_company_id) VALUES
-  ('Fake Title', 'http://www.fakereference.com', 'Fake Reference', 'This is a long fake description.', 10, 1, 1);
+INSERT INTO
+  companies
+VALUES
+  (1, 'ESRI', 'Esri', 'software'),
+  (2, 'Google', 'Google', 'software'),
+  (3, 'Microsoft', 'Microsoft', 'software'),
+  (4, 'Apple', 'Apple_Inc.', 'software'),
+  (5, 'Mercadona', 'Mercadona', 'retail'),
+  (6, 'Dia', 'Dia_(supermarket_chain)', 'retail'),
+  (7, 'Aldi', 'Aldi', 'retail'),
+  (8, 'Carrefour', 'Carrefour', 'retail'),
+  (9, 'Consum', NULL, 'retail'),
+  (10, 'Masymas', NULL, 'retail'),
+  (11, 'Eroski', 'Eroski', 'retail'),
+  (12, 'Lidl', 'Lidl', 'retail'),
+  (13, 'E.Leclerc', 'E.Leclerc', 'retail');
 
-INSERT INTO rating_records (rating, fk_created_by, fk_company_id) VALUES
-  (4, 1, 1);
+INSERT INTO
+  evidence_records
+VALUES
+  (1, 'Fake Title', 'http://www.fakereference.com', 'Fake Reference', 'This is a long fake description.', 10, 1, 1),
+  (2, 'CO2 Emissions', 'https://research-methodology.net/apple-corporate-social-responsibility-csr/', 'Apple Corporate Social Responsibility (CSR)', 'The company’s CO2 emissions per product has been consistently decreasing during the last four years to reach 114.2 kg in 2015.', 10, 1, 4);
+
+INSERT INTO
+  rating_records
+VALUES
+  (1, 4, 1, 1);
